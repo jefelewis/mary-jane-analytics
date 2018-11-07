041318878f2e850682c3d921e1db210422ef2ba8
Jeff Lewis <jeffylew77@gmail.com>
2018-11-07 10:58:59 -0800

    [Dev] Adding comments

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
+// Imports: Dependencies
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// Your top level component
+// Imports: React Application
 import App from './App'
 
-// Export your top level component as JSX (for static rendering)
+// Exports
 export default App
 
-// Render your app
+// Render React Application
 if (typeof document !== 'undefined') {
   const renderMethod = module.hot ? ReactDOM.render : ReactDOM.hydrate || ReactDOM.render
   const render = Comp => {
     renderMethod(<Comp />, document.getElementById('root'))
   }
 
-  // Render!
+  // Render
   render(App)
 }
