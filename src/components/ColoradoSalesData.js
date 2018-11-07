93ebd42ac10e039a59a83f7ffa6aac0a73411614
Jeff Lewis <jeffylew77@gmail.com>
2018-11-07 13:26:18 -0800

    [Dev] Changing file name + import

--- src/components/ColoradoSalesData.js
+++ src/components/ColoradoSalesData.js
@@ -8,7 +8,28 @@ const coloradoSalesdata = {
   labels: [2014, 2015, 2016, 2017],
   datasets: [
     {
-      label: 'Colorado Marijuana Sales Data',
+      label: 'Alaska',
+      data: [null, null, null, null],
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+    },
+    {
+      label: 'Colorado',
       data: [683523739, 995591255, 1307203473, 1507702219],
       fill: false,
       lineTension: 0.1,
@@ -31,8 +52,8 @@ const coloradoSalesdata = {
   ],
 }
 
-// React Component: Colorado Financial Data
-const ColoradoFinancialData = () => (
+// React Component: Colorado Sales Data
+const ColoradoSalesData = () => (
   <div>
     <h1>Marijuana Sales</h1>
     <Line
@@ -59,4 +80,4 @@ const ColoradoFinancialData = () => (
 )
 
 // Exports
-export default ColoradoFinancialData
+export default ColoradoSalesData
