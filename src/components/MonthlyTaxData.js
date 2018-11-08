c551802921b525324aef6d652694c92ba42b9e96
Jeff Lewis <jeffylew77@gmail.com>
2018-11-08 12:29:44 -0800

    [Fix] Removing imports

--- src/components/MonthlyTaxData.js
+++ src/components/MonthlyTaxData.js
@@ -1,8 +1,6 @@
 // Imports: Dependencies
 import React, { Component } from 'react'
 import { Line } from 'react-chartjs-2'
-import { renderStatic } from 'react-helmet';
-import { runInThisContext } from 'vm';
 
 // ChartJS: Colorado Tax Data
 const monthlyTaxdata = {
@@ -11,8 +9,7 @@ const monthlyTaxdata = {
     'January (2015)', 'February (2015)', 'March (2015)', 'April (2015)', 'June (2015)', 'July (2015)', 'August (2015)', 'September (2015)', 'October (2015)', 'November (2015)', 'December (2015)',
     'January (2016)', 'February (2016)', 'March (2016)', 'April (2016)', 'June (2016)', 'July (2016)', 'August (2016)', 'September (2016)', 'October (2016)', 'November (2016)', 'December (2016)',
     'January (2017)', 'February (2017)', 'March (2017)', 'April (2017)', 'June (2017)', 'July (2017)', 'August (2017)', 'September (2017)', 'October (2017)', 'November (2017)', 'December (2017)',
-    'January (2018)', 'February (2018)', 'March (2018)', 'April (2018)', 'June (2018)', 'July (2018)', 'August (2018)', 'September (2018)', 'October (2018)', 'November (2018)', 'December (2018)'
-
+    'January (2018)', 'February (2018)', 'March (2018)', 'April (2018)', 'June (2018)', 'July (2018)', 'August (2018)', 'September (2018)', 'October (2018)', 'November (2018)', 'December (2018)',
   ],
   datasets: [
     {
