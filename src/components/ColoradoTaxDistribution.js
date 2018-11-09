ccb405c011f0a840130bbad9dd1034b4c87a6a55
Jeff Lewis <jeffylew77@gmail.com>
2018-11-09 00:51:32 -0800

    [Dev] Adding Tax distribution components

---
+++ src/components/ColoradoTaxDistribution.js
@@ -0,0 +1,80 @@
+// Imports: Dependencies
+import React, { Component } from 'react'
+
+// React Component: Tax Distribution
+class TaxDistribution extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+    }
+  }
+
+
+  render () {
+    return (
+      <div>
+        <br></br>
+        <h1>How Is The Marijuana Tax Revenue Allocated?</h1>
+
+        <h2>Colorado</h2>
+        <h3>2014 - 2015</h3>
+        <ul>
+          <li><b>General Fund: </b> $27.7 Million Dollars</li>
+          <li><b>Distribution to Local Governments: </b> $5.7 Million Dollars</li>
+        </ul>
+
+        <h3>2015 - 2016</h3>
+        <ul>
+          <li><b>Public School Capital Construction Assistance Fund (Any excess funds are sent to the Public School Fund):</b> $40 Million Dollars</li>
+          <li><b>Public School Fund:</b> $2.5 Million Dollars</li>
+        </ul>
+
+        <h3>2016 - 2017</h3>
+        <ul>
+          <li><b>Public School Capital Construction Assistance Fund (Any excess funds are sent to the Public School Fund):</b> $40 Million Dollars</li>
+          <li><b>Public School Fund:</b> $31.6 Million Dollars</li>
+        </ul>
+
+        <div>
+          <h3>2017 - 2018</h3>
+          <h4>Marijuana Tax Cash Fund</h4>
+          <ul>
+            <li><b>Agriculture:</b> $ Dollars</li>
+            <li><b>Education:</b> $ Dollars</li>
+            <li><b>Governor:</b> $ Dollars</li>
+            <li><b>Health Care Policy & Financing:</b> $ Dollars</li>
+            <li><b>Higher Education:</b> $ Dollars</li>
+            <li><b>Human Services:</b> $ Dollars</li>
+            <li><b>Judicial:</b> $ Dollars</li>
+            <li><b>Labor & Employment:</b> $ Dollars</li>
+            <li><b>Law:</b> $ Dollars</li>
+            <li><b>Local Affairs:</b> $ Dollars</li>
+            <li><b>Public Health & Environment:</b> $ Dollars</li>
+            <li><b>Public Safety:</b> $ Dollars</li>
+            <li><b>Regulatory Agencies:</b> $ Dollars</li>
+            <li><b>Transportation:</b> $ Dollars</li>
+            <li><b>Appropriations in Other 2017 Legislation:</b> $ Dollars</li>
+          </ul>
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+      </div>
+    )
+  }
+}
+
+// Exports
+export default TaxDistribution
---
+++ src/components/WashingtonTaxDistribution.js
@@ -0,0 +1,130 @@
+// Imports: Dependencies
+import React, { Component } from 'react'
+import { Bar } from 'react-chartjs-2'
+
+// ChartJS: Washington
+const washingtonDistribution = {
+  labels: [
+    '2017',
+  ],
+  datasets: [
+    {
+      label: 'General Fund',
+      data: [
+        96600000,
+      ],
+      borderColor: 'rgba(255,99,132,1)',
+      borderWidth: 1,
+      stack: '1',
+      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+      hoverBorderColor: 'rgba(255,99,132,1)',
+    },
+    {
+      label: 'Basic Health',
+      data: [
+        145700000,
+      ],
+      borderColor: 'rgba(255,99,132,1)',
+      borderWidth: 1,
+      stack: '2',
+      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+      hoverBorderColor: 'rgba(255,99,132,1)',
+    },
+    {
+      label: 'Cities & Counties',
+      data: [
+        55000000,
+      ],
+      borderColor: 'rgba(255,99,132,1)',
+      borderWidth: 1,
+      stack: '3',
+      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+      hoverBorderColor: 'rgba(255,99,132,1)',
+    },
+    {
+      label: 'Education, Prevention',
+      data: [
+        44100000,
+      ],
+      borderColor: 'rgba(255,99,132,1)',
+      borderWidth: 1,
+      stack: '4',
+      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+      hoverBorderColor: 'rgba(255,99,132,1)',
+    },
+    {
+      label: 'Research',
+      data: [
+        2700000,
+      ],
+      borderColor: 'rgba(255,99,132,1)',
+      borderWidth: 1,
+      stack: '5',
+      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+      hoverBorderColor: 'rgba(255,99,132,1)',
+    },
+    {
+      label: 'Other',
+      data: [
+        17400000,
+      ],
+      borderColor: 'rgba(255,99,132,1)',
+      borderWidth: 1,
+      stack: '6',
+      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+      hoverBorderColor: 'rgba(255,99,132,1)',
+    },
+  ],
+}
+
+
+// React Component: Tax Distribution
+class TaxDistribution extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+    }
+  }
+
+
+  render () {
+    return (
+      <div>
+        <Bar
+          data={washingtonDistribution}
+          options={{
+            maintainAspectRatio: true,
+            scaleBeginAtZero: true,
+            responsive: true,
+            scales: {
+              scales: {
+                xAxes: [{
+                    stacked: true,
+                }],
+                yAxes: [{
+                    stacked: true,
+                }]
+              }
+            }
+          }}
+        />
+
+
+        <h2>Washington</h2>
+        <h3>2017</h3>
+        <ul>
+          <li><b>General Fund:</b> $96.6 Million Dollars</li>
+          <li><b>Basic Health :</b> $145.7 Million Dollars</li>
+          <li><b>Cities & Counties:</b> $55.5 Million Dollars</li>
+          <li><b>Education, Prevention:</b> $44.1 Million Dollars</li>
+          <li><b>Research:</b> $2.7 Million Dollars</li>
+          <li><b>Other:</b> $17.4 Million Dollars</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+// Exports
+export default TaxDistribution
